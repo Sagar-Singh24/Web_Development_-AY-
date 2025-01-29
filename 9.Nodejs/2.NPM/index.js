59688d9766d48e108b8c544106adf4f8bcb023f3
Sagar Singh <sssssagarsingh24@gmail.com>
2025-01-29 23:44:54 +0530

    Add files via upload

---
+++ 9.Nodejs/2.NPM/index.js
@@ -0,0 +1,8 @@
+var superHeros = require("Superheroes");
+var superVillain = require("Supervillains");
+
+var mySuperHeroes = superHeros.random();
+var mySuperVillains = superVillain.random();
+
+console.log(mySuperHeroes);
+console.log(mySuperVillains);
